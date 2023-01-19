561e42820b3c14ca0ed933d9760a8987e1495792
higa <codehiga@gmail.com>
2023-01-19 18:45:24 -0300

    fix: removido repository de send to queue

--- src/usecases/send-to-queue.ts
+++ src/usecases/send-to-queue.ts
@@ -1,4 +1,3 @@
-import { MalaDiretaRepository } from "@/data/contracts/mala-direta-repository";
 import { InvalidEmailError } from "@/entities/errors/invalid-email-error";
 import { InvalidNameError } from "@/entities/errors/invalid-name-error";
 import { InvalidTemplateCode } from "@/entities/errors/invalid-template-code";
@@ -8,17 +7,15 @@ import { QueueContract } from "@/services/contracts/queue";
 import { Either, left } from "@/shared/either";
 
 export class SendToQueue {
-  constructor(private repository: MalaDiretaRepository, private fila: QueueContract) { }
+  constructor(private fila: QueueContract) { }
 
-  async execute(data: CreateMalaDiretaModel[]) : Promise<Either<InvalidEmailError | InvalidNameError | InvalidTemplateCode, void>> {
+  async execute(data: CreateMalaDiretaModel[], queue: string) : Promise<Either<InvalidEmailError | InvalidNameError | InvalidTemplateCode, void>> {
     for(let maladiretaData of data) {
       const maladiretaOrError = MalaDireta.create(maladiretaData);
       if(maladiretaOrError.isLeft()) {
         return left(maladiretaOrError.value);
       }
-      await this.fila.send("email", maladiretaOrError.value);
-      await this.repository.save(maladiretaOrError.value);
+      await this.fila.send(queue, maladiretaOrError.value);
     }
-    return;
   }
 }
