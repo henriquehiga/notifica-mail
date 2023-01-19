b1d40948ef23c72913359bb374577fec9c7399b3
higa <codehiga@gmail.com>
2023-01-19 18:13:54 -0300

    fix: testes de fila corrigidos

--- tests/services/rabbit-queue.test.ts
+++ tests/services/rabbit-queue.test.ts
@@ -1,16 +1,23 @@
 import { RabbitFilaImpl } from "@/services/rabbit-queue-impl";
 
 describe('RabbitMQ', () => {
-  it('espero inserir e resgatar a quantidade de mensagens que eu estipular', async () => {
+  it('espero inserir mensagens na fila', async () => {
     const fila = new RabbitFilaImpl();
-    const nomeFila = 'email';
+    const nomeFila = 'teste-fila';
     for(let i = 0; i < 20; i++) {
       await fila.send(nomeFila, {
         name: "name " + i,
         email: "validemail@mail.com"
       });
     }
-    const mensagens = await fila.get(nomeFila, 20);
-    expect(mensagens.length).toBe(20);
+    const quantidadeMensagensFila = await fila.count(nomeFila);
+    expect(quantidadeMensagensFila).toBe(20);
+  });
+
+  it('espero resgatar a quantidade de mensagens que foi estipulada', async () => {
+    const fila = new RabbitFilaImpl();
+    const nomeFila = 'teste-fila';
+    const mensagens = await fila.get(nomeFila, 2);
+    expect(mensagens.length).toBe(2);
   });
 });
