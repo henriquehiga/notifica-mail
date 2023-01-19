70e7f63693ef1c17941f96f225c5584f59a583f3
Higa <codehiga@gmail.com>
2023-01-19 09:49:05 -0300

    chore: initial config

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+module.exports = {
+  roots: ["<rootDir>/tests"],
+  coverageDirectory: "coverage",
+  collectCoverageFrom: [
+    "<rootDir>/src/**/*.ts",
+    "!**/tests/**",
+    "!**/config/**",
+  ],
+  transform: {
+    ".+\\.ts$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^@test/(.*)$": "<rootDir>/tests/$1",
+  },
+};
