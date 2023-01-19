264edb2f9bb493d6148bb59b18796aa33071994f
higa <codehiga@gmail.com>
2023-01-19 19:26:57 -0300

    refact: melhorado input de mala direta

--- tests/data/mala-direta-repository.test.ts
+++ tests/data/mala-direta-repository.test.ts
@@ -1,19 +1,24 @@
 import { MalaDiretaRepository } from "@/data/contracts/mala-direta-repository";
 import { MalaDireta } from "@/entities/mala-direta";
+import { CreateMalaDiretaModel } from "@/entities/models/create-mala-direta";
 import { InMemoryMalaDiretaRepository } from "./memory-mala-direta-repository";
 
 describe("MalaDireta Repository", () => {
   test("espero salvar uma mala direta", async () => {
     let malasdiretas = [];
     const repository : MalaDiretaRepository = new InMemoryMalaDiretaRepository(malasdiretas);
-    const maladireta = MalaDireta.create({
-      email: "validemail@mail.com",
-      name: "valid name"
-    }, {
-      templateCode: "abc-123"
-    }).value as MalaDireta;
+    const input: CreateMalaDiretaModel = {
+      cliente : {
+        name: "Valid Name",
+        email: "validemail@mail.com"
+      },
+      maladiretaData : {
+        templateCode : "abc-123"
+      }
+    }
+    const maladireta = MalaDireta.create(input).value as MalaDireta;
     repository.save(maladireta);
-    const malasdiretasCliente = await repository.getByEmail('validemail@mail.com');
+    const malasdiretasCliente = await repository.getByEmail("validemail@mail.com");
     expect(malasdiretasCliente.length).toBe(1);
   })
 })
