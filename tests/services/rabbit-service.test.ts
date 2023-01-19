ac9351f35bf0d31364e8efa801e3b7fe7cfb92a2
higa <codehiga@gmail.com>
2023-01-19 19:26:29 -0300

    refact: renomeado arquivo de teste do rabbitservice

---
+++ tests/services/rabbit-service.test.ts
@@ -0,0 +1,36 @@
+import { QueueContract } from "@/services/contracts/queue";
+import { RabbitFilaImpl } from "@/services/rabbit-queue-impl";
+
+describe('RabbitMQ Service', () => {
+
+  const queue: QueueContract = new RabbitFilaImpl(); 
+  const nomeFila = "teste/rabbit-service";
+
+  afterEach(async () => {
+    await queue.deleteQueue(nomeFila);
+  });
+
+  it('espero inserir mensagens na fila', async () => {
+    for(let i = 0; i < 20; i++) {
+      await queue.send(nomeFila, {
+        name: "valid name " + i,
+        email: "validemail@mail.com"
+      });
+    }
+    const quantidadeMensagensFila = await queue.count(nomeFila);
+    expect(quantidadeMensagensFila).toBe(20);
+  });
+
+  it('espero resgatar a quantidade de mensagens que foi estipulada', async () => {
+    await queue.send(nomeFila, {
+      name: "valid name 1",
+      email: "validemail1@mail.com"
+    });
+    await queue.send(nomeFila, {
+      name: "valid name 2",
+      email: "validemail2@mail.com"
+    });
+    const mensagens = await queue.get(nomeFila, 2);
+    expect(mensagens.length).toBe(2);
+  });
+});
