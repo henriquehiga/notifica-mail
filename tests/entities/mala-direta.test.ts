2816bd978e92ea7c2ad2d5444c3dfe26d08fe447
higa <codehiga@gmail.com>
2023-01-19 19:27:23 -0300

    refact: melhorado input de mala direta

--- tests/entities/mala-direta.test.ts
+++ tests/entities/mala-direta.test.ts
@@ -1,39 +1,46 @@
 import { MalaDireta } from "@/entities/mala-direta";
+import { CreateMalaDiretaModel } from "@/entities/models/create-mala-direta";
 
 describe("Entidade mala direta", () => {
   test("Retorna erro caso nome seja inválido", () => {
-    const inputClienteData = {
-      name: "invalid_name",
-      email: "validemail@mail.com"
+    const input: CreateMalaDiretaModel = {
+      cliente : {
+        name: "invalid_name",
+        email: "validemail@mail.com"
+      },
+      maladiretaData : {
+        templateCode : "abc-123"
+      }
     }
-    const inputTemplateData = {
-      templateCode : "abc-123"
-    }
-    const error = MalaDireta.create(inputClienteData, inputTemplateData).value as Error;
+    const error = MalaDireta.create(input).value as Error;
     expect(error.name).toBe("InvalidNameError");
   })
 
   test("Retorna erro caso email seja inválido", () => {
-    const inputClienteData = {
-      name: "Valid Name",
-      email: "invalid email@mail.com"
+    const input: CreateMalaDiretaModel = {
+      cliente : {
+        name: "valid name",
+        email: "invlaidemailmail.com"
+      },
+      maladiretaData : {
+        templateCode : "abc-123"
+      }
     }
-    const inputTemplateData = {
-      templateCode : "abc-123"
-    }
-    const error = MalaDireta.create(inputClienteData, inputTemplateData).value as Error;
+    const error = MalaDireta.create(input).value as Error;
     expect(error.name).toBe("InvalidEmailError");
   })
 
   test("Retorna erro caso não envie template id", () => {
-    const inputClienteData = {
-      name: "Valid Name",
-      email: "validemail@mail.com"
+    const input: CreateMalaDiretaModel = {
+      cliente : {
+        name: "valid name",
+        email: "validemail@mail.com"
+      },
+      maladiretaData : {
+        templateCode : ""
+      }
     }
-    const inputTemplateData = {
-      templateCode : ""
-    }
-    const error = MalaDireta.create(inputClienteData, inputTemplateData).value as Error;
+    const error = MalaDireta.create(input).value as Error;
     expect(error.name).toBe("InvalidTemplateCode");
   })
 })
