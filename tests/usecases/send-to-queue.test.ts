12dc13c46097fb53174a142c13f141b1f7db00a5
Higa <codehiga@gmail.com>
2023-01-21 13:33:13 -0300

    test: utilizando exchanges para inserir dados

--- tests/usecases/send-to-queue.test.ts
+++ tests/usecases/send-to-queue.test.ts
@@ -1,12 +1,15 @@
 import { QueueContract } from "@/adapters/contracts/queue";
 import { RabbitAdapterImpl } from "@/adapters/rabbit-adapter-impl";
+import { QUEUE_CONSTANTS } from "@/adapters/utils/constants";
 import { CreateMalaDiretaModel } from "@/entities/models/create-mala-direta";
 import { SendToQueue } from "@/usecases/send-to-queue";
 
 describe("SendToQueue Usecase", () => {
   const queue: QueueContract = new RabbitAdapterImpl(); 
-  const nomeFila = "fila-teste";
-
+  const nomeFila = QUEUE_CONSTANTS.FILA_TESTES;
+  const exchange = QUEUE_CONSTANTS.EXCHANGE;
+  const routingKey = QUEUE_CONSTANTS.ROUTING_KEY;
+  
   // afterEach(async () => {
   //   await queue.deleteQueue(nomeFila);
   // });
@@ -23,8 +26,8 @@ describe("SendToQueue Usecase", () => {
       }
     }
     const input = [ maladiretaData ];
-    await usecase.execute(input, nomeFila);
-    let data = await queue.get(nomeFila, 1);
+    await usecase.execute(input, exchange, routingKey);
+    let data = await queue.get(exchange, nomeFila, routingKey);
     data = JSON.parse(data[0]);
     expect(data).toEqual(maladiretaData);
   })
@@ -41,26 +44,7 @@ describe("SendToQueue Usecase", () => {
       }
     }
     const input = [ maladiretaData ];
-    const error = (await usecase.execute(input, nomeFila)).value as Error;
+    const error = (await usecase.execute(input, exchange, routingKey)).value as Error;
     expect(error.name).toBe("InvalidNameError");
-  })
-
-  test("Popula RABBIT", async () => {
-    const usecase = new SendToQueue(queue);
-    const input = [];
-    for (let index = 0; index < 50; index++) {
-      const maladiretaData : CreateMalaDiretaModel = {
-        cliente: {
-          email: "cary2@ethereal.email",
-          name: "Valid Name"
-        },
-        maladiretaData: {
-          templateCode: "abc-123"
-        }
-      }
-      input.push(maladiretaData);
-    }
-    const response = await usecase.execute(input, nomeFila);
-    expect(response.isRight()).toBeTruthy();
   })
 })
