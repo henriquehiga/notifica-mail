fb0778a53090cab4abee9410a7a127d83acf7d1a
Higa <codehiga@gmail.com>
2023-01-21 17:36:08 -0300

    feat: try catch adicionado na impl na fila

--- src/adapters/rabbit-adapter-impl.ts
+++ src/adapters/rabbit-adapter-impl.ts
@@ -9,51 +9,84 @@ export class RabbitAdapterImpl implements QueueContract {
   constructor() { }
 
   async deleteQueue(queue: string): Promise<void> {
-    await this.connect();
-    await this.channel.deleteQueue(queue);
-    await this.close();
+    try {
+      await this.connect();
+      await this.channel.deleteQueue(queue);
+      await this.close();
+    } catch(err) {
+      throw new Error();
+    }
   }
 
   private async connect() {
-    let credentials = `amqp://${cfg.username}:${cfg.password}@${cfg.hostname}:${cfg.port}`;
-    const connection = await amqp.connect(credentials);
-    this.connection = connection;
-    this.channel = await this.connection.createChannel();
+    try {
+      let credentials = `amqp://${cfg.username}:${cfg.password}@${cfg.hostname}:${cfg.port}`;
+      const connection = await amqp.connect(credentials);
+      this.connection = connection;
+      this.channel = await this.connection.createChannel();
+    } catch(err) {
+      throw new Error();
+    }
   }
 
   public async send(exchange: string, routingKey: string, data: any) {
-    await this.connect();
-    await this.channel.assertExchange(exchange, 'direct', { durable: true });
-    const payload = data;
-    this.channel.publish(exchange, routingKey, Buffer.from(JSON.stringify(payload)));
-    await this.close();
+    try {
+      await this.connect();
+      await this.channel.assertExchange(exchange, 'direct', { durable: true });
+      const payload = data;
+      this.channel.publish(exchange, routingKey, Buffer.from(JSON.stringify(payload)));
+      await this.close();
+    } catch(err) {
+      throw new Error();
+    }
+  }
+
+  public async ack(message: any) {
+    try {
+      this.channel.ack(message);
+    } catch(err) {
+      throw new Error();
+    }
   }
 
   public async get(exchange: string, queue: string, routingKey: string) {
-    await this.connect();
-    await this.channel.assertExchange(exchange, 'direct', { durable: true });
-    await this.channel.assertQueue(queue, { durable: true });
-    await this.channel.bindQueue(queue, exchange, routingKey);
-    let data = [];
-    await this.channel.consume(queue, async (msg) => {
-      if (msg) {
-        data.push(msg.content.toString());
-        this.channel.ack(msg);
-      }
-    });
-    await this.close();
-    return data;
+    try {
+      await this.connect();
+      await this.channel.assertExchange(exchange, 'direct', { durable: true });
+      await this.channel.assertQueue(queue, { durable: true });
+      await this.channel.prefetch(20);
+      await this.channel.bindQueue(queue, exchange, routingKey);
+      let data = [];
+      await this.channel.consume(queue, async (msg) => {
+        if (msg) {
+          data.push(msg.content.toString());
+          this.channel.ack(msg);
+        }
+      });
+      await this.close();
+      return data;
+    } catch(err) {
+      throw new Error();
+    }
   }
 
   public async count(queue: string) {
-    await this.connect();
-    const queueInfo = await this.channel.checkQueue(queue);
-    await this.close();
-    return queueInfo.messageCount;
+    try {
+      await this.connect();
+      const queueInfo = await this.channel.checkQueue(queue);
+      await this.close();
+      return queueInfo.messageCount;
+    } catch(err) {
+      throw new Error();
+    }
   }
 
   public async close() {
-    await this.channel.close();
-    await this.connection.close();
+    try {
+      await this.channel.close();
+      await this.connection.close();
+    } catch(err) {
+      throw new Error();
+    }
   }
 }
