ac0f84d474502e59dbe10c7f604993d2463d8f84
Higa <codehiga@gmail.com>
2023-01-24 19:42:02 -0300

    test: teste rabbit adapter

--- tests/adapters/rabbit-adapter.test.ts
+++ tests/adapters/rabbit-adapter.test.ts
@@ -1,10 +1,12 @@
 import { QueueContract } from "@/adapters/contracts/queue";
 import { RabbitAdapterImpl } from "@/adapters/rabbit-adapter-impl";
+import { QUEUE_CONSTANTS } from "@/adapters/utils/constants";
 
 describe('RabbitMQ Adapter', () => {
-
   const queue: QueueContract = new RabbitAdapterImpl(); 
-  const nomeFila = "teste/rabbit-adapter";
+  const nomeFila = QUEUE_CONSTANTS.FILA;
+  const nomeExchange = QUEUE_CONSTANTS.EXCHANGE;
+  const nomeRoutingKey = QUEUE_CONSTANTS.ROUTING_KEY;
 
   afterEach(async () => {
     await queue.deleteQueue(nomeFila);
@@ -12,25 +14,12 @@ describe('RabbitMQ Adapter', () => {
 
   it('espero inserir mensagens na fila', async () => {
     for(let i = 0; i < 20; i++) {
-      await queue.send(nomeFila, {
+      await queue.send(nomeExchange, nomeRoutingKey, {
         name: "valid name " + i,
         email: "validemail@mail.com"
       });
     }
     const quantidadeMensagensFila = await queue.count(nomeFila);
     expect(quantidadeMensagensFila).toBe(20);
-  });
-
-  it('espero resgatar a quantidade de mensagens que foi estipulada', async () => {
-    await queue.send(nomeFila, {
-      name: "valid name 1",
-      email: "validemail1@mail.com"
-    });
-    await queue.send(nomeFila, {
-      name: "valid name 2",
-      email: "validemail2@mail.com"
-    });
-    const mensagens = await queue.get(nomeFila, 2);
-    expect(mensagens.length).toBe(2);
   });
 });
